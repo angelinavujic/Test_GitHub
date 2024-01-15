022577f0d6639ea63ef0dcd95d3419e977fca499
angelinavujic <angelina.vujic@hak-reutte.ac.at>
2024-01-15 10:34:26 +0100

    both classes online

--- src/example/FirstTryOut.java
+++ src/example/FirstTryOut.java
@@ -3,7 +3,9 @@ package example;
 public class FirstTryOut {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+		System.out.println("Hello my name is Angelina");
 
 	}
 
