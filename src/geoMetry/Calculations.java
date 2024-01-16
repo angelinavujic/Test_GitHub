f91e6cc4ac4531d7f8c462072e39970d5201923d
angelinavujic <angelina.vujic@hak-reutte.ac.at>
2024-01-16 09:38:43 +0100

    methods, getters and setters and attributes have been created

--- src/geoMetry/Calculations.java
+++ src/geoMetry/Calculations.java
@@ -1,5 +1,29 @@
 package geoMetry;
 
 public abstract class Calculations {
+	
+	private double y;
+	private double x;
+	
+	
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	
+	public abstract double calculateCircumference();
+	
+	public abstract double calculateArea();
+	
+	
+	
 
 }
