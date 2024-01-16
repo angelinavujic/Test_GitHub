33aaec98aaa9f290a5c4ce4591d03045023004e4
angelinavujic <angelina.vujic@hak-reutte.ac.at>
2024-01-16 09:45:10 +0100

    Class Rectangle was filled with methods calculateCricumference and calculateArea, constructor and toString

--- src/geoMetry/Rectangle.java
+++ src/geoMetry/Rectangle.java
@@ -1,5 +1,55 @@
 package geoMetry;
 
-public class Rectangle {
+public class Rectangle extends Calculations{
+	
+	private double length;
+	private double width;
+	
+	public Rectangle() {
+		super();
+		
+	}
+	
+	public Rectangle(double x, double y, double length, double width) {
+		super();
+		this.length = length;
+		this.width = width;
+	}
+
+
+
+	public double getLength() {
+		return length;
+	}
+
+
+
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+
+
+	public double getWidth() {
+		return width;
+	}
+
+
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+
+
+	@Override
+	public double calculateCircumference() {
+		return (2*this.length) + (2*this.width);
+	}
+
+	@Override
+	public double calculateArea() {
+		return this.length *this.width;
+	}
 
 }
