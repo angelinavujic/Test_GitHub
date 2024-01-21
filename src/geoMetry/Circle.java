fd730259712738b792b0ba22fc8b850b02f7769c
angelinavujic <angelina.vujic@hak-reutte.ac.at>
2024-01-21 23:07:31 +0100

    class circle completed with constructor and methods

--- src/geoMetry/Circle.java
+++ src/geoMetry/Circle.java
@@ -1,5 +1,26 @@
 package geoMetry;
 
-public class Circle {
+public class Circle extends Calculations {
+	
+	private double radius;
+
+	
+	public Circle(double y, double x, double radius) {
+		super(y, x);
+		this.radius = radius;
+	}
+	
+	@Override
+	public double calculateArea() {
+		return Math.PI * radius * radius;
+		
+	}
+
+	@Override
+	public double calculateCircumference() {
+		return 2 * Math.PI * radius;
+	}
+	
+	
 
 }
