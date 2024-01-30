5a51be99e1d8449aa7b97ed4c71df00a5ea67ffd
angelinavujic <angelina.vujic@hak-reutte.ac.at>
2024-01-30 09:35:22 +0100

    updated constructor

--- src/theSocialNetwork/Messages.java
+++ src/theSocialNetwork/Messages.java
@@ -1,18 +1,20 @@
 package theSocialNetwork;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
+
 
 public class Messages extends MessageFacts {
 	
 	private String messageText;
 
 	
-	public Messages(String username, LocalDate postingTime, ArrayList<String> comments, int likes, String messageText) {
-		super(username, postingTime, comments, likes);
-		this.messageText = messageText;
+	
 		
+	public Messages(String username, String messageText) {
+		super(username);
+		this.messageText = messageText;
 	}
+
+	
 
 	
 	public String getMessageText() {
