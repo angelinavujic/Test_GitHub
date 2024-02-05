becfcccc0a2f0b6bae8ff7553af816caa36fd70d
angelinavujic <angelina.vujic@hak-reutte.ac.at>
2024-02-05 11:33:50 +0100

    added constructor + getter/setters

--- src/theSocialNetwork/NewsFeed.java
+++ src/theSocialNetwork/NewsFeed.java
@@ -5,6 +5,25 @@ import java.util.ArrayList;
 public class NewsFeed {
 	
 	private ArrayList<MessageFacts>feedPosts;
+
+	public NewsFeed(ArrayList<MessageFacts> feedPosts) {
+		super();
+		this.feedPosts = feedPosts;
+	}
+
+	
+	
+	public ArrayList<MessageFacts> getFeedPosts() {
+		return feedPosts;
+	}
+
+	public void setFeedPosts(ArrayList<MessageFacts> feedPosts) {
+		this.feedPosts = feedPosts;
+	}
+	
+	
+	
+	
 	
 
 }
