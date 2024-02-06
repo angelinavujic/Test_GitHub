dea93288d82607afef82ab7e06372b91790604ff
angelinavujic <angelina.vujic@hak-reutte.ac.at>
2024-02-06 09:38:06 +0100

    new test functions created

--- src/theSocialNetwork/Tests.java
+++ src/theSocialNetwork/Tests.java
@@ -5,11 +5,17 @@ public class Tests {
 	public static void main(String[] args) {
 
 
-		Messages m1 = new Messages("DavidG", "Hallooo!");
-		System.out.println(m1);
+		Messages m1 = new Messages("AngelinaV", "Hallooo!");
 		
 		PhotoPosts p1 = new PhotoPosts("DavidG", "png", "HELP", "[photo]");
-		System.out.println(p1);
+
+		
+		NewsFeed n1 = new NewsFeed();
+		
+		n1.addToNewsFeed(m1);
+		n1.addToNewsFeed(p1);
+		//n1.printNewsFeed();
+		n1.printPostPerName("DavidG");
 
 	}
 
